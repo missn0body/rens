01bfc738282c2ddde143bea0b18e1fd6fe9afb11
missn0body <hulk128t@gmail.com>
2024-06-03 11:39:21 -0500

    Fixed some things regarding command line parsing

--- 1.0.0/rens.c
+++ 1.0.0/rens.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // POSIX libraries
 #include <sys/stat.h>
@@ -15,10 +16,12 @@ typedef char flag_t;
 static flag_t status = 0x00;
 enum : char
 {
-	VERBOSE = (1 << 0),
-	PREVIEW = (1 << 1),
-	NOBAIL = (1 << 2),
-	NOPAT = (1 << 3)
+	VERBOSE  = (1 << 0),
+	PREVIEW  = (1 << 1),
+	NOBAIL   = (1 << 2),
+	NOPAT    = (1 << 3),
+	QUIET    = (1 << 4),
+	FIRSTSUF = (1 << 5)
 };
 
 static const char *defdir = ".";
@@ -31,9 +34,9 @@ static constexpr short defwidth = 3;
 
 inline bool check_if_dir(const struct stat *input) { return (S_ISDIR(input->st_mode)); }
 
-static inline void   set(flag_t *in, char what) { *in |=   what; }
-static inline void unset(flag_t *in, char what) { *in &= (~what); }
-static inline bool  test(flag_t *in, char what) { return ((*in & what) == 0); }
+static inline void   set(flag_t *in, char what) { (*in) |=   what; }
+static inline void unset(flag_t *in, char what) { (*in) &= (~what); }
+static inline bool  test(flag_t *in, char what) { return (((*in) & what) != 0); }
 
 void usage(void) { printf("usage!\n"); return; }
 
@@ -69,15 +72,15 @@ int main(int argc, char *argv[])
 	int c;
 	while(--argc > 0 && (*++argv)[0] != '\0')
 	{
-		if((*argv)[0] != '-')
+		if((*argv)[0] != '-' && !isdigit(**argv))
 		{
-			if(pattern[0] != '\0')
+			if(dirname[0] != '\0')
 			{
 				fprintf(stderr, "%s: discarded program input -- \"%s\"\n", programname, *argv);
 				continue;
 			}
 
-			strncpy((dirname[0] == '\0') ? dirname : pattern, *argv, (dirname[0] == '\0') ? sizeof(dirname) : sizeof(pattern));
+			strncpy((pattern[0] == '\0') ? pattern : dirname, *argv, (pattern[0] == '\0') ? sizeof(pattern) : sizeof(dirname));
 		}
 
 		if((*argv)[0] == '-')
@@ -93,11 +96,30 @@ int main(int argc, char *argv[])
 				if(strcmp((*argv) + 2, "version") == 0) { version(); exit(EXIT_SUCCESS); }
 				if(strcmp((*argv) + 2, "width")   == 0)
 				{
-					suffix_width = atoi(*argv);
-					exit(EXIT_SUCCESS);
+					if(argv[1] == nullptr)
+					{
+						fprintf(stderr, "%s: no argument for width\n", programname);
+						exit(EXIT_FAILURE);
+					}
+
+					suffix_width = atoi(argv[1]);
+					continue;
 				}
-				if(strcmp((*argv) + 2, "no-bail") == 0) { set(&status, NOBAIL);  exit(EXIT_SUCCESS); }
-				if(strcmp((*argv) + 2, "preview") == 0) { set(&status, PREVIEW); exit(EXIT_SUCCESS); }
+				if(strcmp((*argv) + 2, "no-bail") == 0) { set(&status, NOBAIL); continue; }
+				if(strcmp((*argv) + 2, "preview") == 0) { set(&status, PREVIEW); continue; }
+				if(strcmp((*argv) + 2, "verbose") == 0) { set(&status, VERBOSE); continue; }
+				if(strcmp((*argv) + 2, "suffix-first") == 0) { set(&status, FIRSTSUF); continue; }
+				if(strcmp((*argv) + 2, "quiet") == 0)
+				{
+					set(&status, QUIET);
+					if(test(&status, VERBOSE))
+					{
+						fprintf(stderr, "%s: quiet flag counters verbose flag\n", programname);
+						unset(&status, VERBOSE);
+					}
+
+					continue;
+				}
 			}
 			while((c = *++argv[0]))
 			{
@@ -106,12 +128,28 @@ int main(int argc, char *argv[])
 				{
 					case 'h': usage(); exit(EXIT_SUCCESS);
 					case 'w':
-						  suffix_width = atoi(*argv);
-						  exit(EXIT_SUCCESS);
+						  if(argv[1] == nullptr)
+						  {
+							fprintf(stderr, "%s: no argument for width\n", programname);
+							exit(EXIT_FAILURE);
+						  }
+
+						  suffix_width = atoi(argv[1]);
 						  break;
 
 					case 'n': set(&status, NOBAIL);  break;
 					case 'p': set(&status, PREVIEW); break;
+					case 'v': set(&status, VERBOSE); break;
+					case 's': set(&status, FIRSTSUF); break;
+					case 'q':
+						  set(&status, QUIET);
+						  if(test(&status, VERBOSE))
+						  {
+							fprintf(stderr, "%s: quiet flag counters verbose flag\n", programname);
+							unset(&status, VERBOSE);
+						  }
+
+						  break;
 					// This error flag can either be set by a
 					// completely unrelated character inputted,
 					// or you managed to put -option instead of
@@ -128,7 +166,7 @@ int main(int argc, char *argv[])
 	// If we did not get a directory name, just look inside current working directory
 	if(dirname[0] == '\0') 			     snprintf(dirname, bufsize, "%s", defdir);
 	if(pattern[0] == '\0')			     set(&status, NOPAT);
-	if(suffix_width <= 1)			     suffix_width = defwidth;
+	if(suffix_width <= 0)			     suffix_width = defwidth;
 	if((dirobj = opendir(dirname)) == nullptr) { perror(programname); exit(EXIT_FAILURE); }
 	// Plus one for forwards slash
 	size_t dirlen = strlen(dirname) + 1, ret = 0;
@@ -136,6 +174,19 @@ int main(int argc, char *argv[])
 	// There's two checks for bailout, with two uses each, so it's more efficient
 	// to just refer to a single bool instead of complicating inline ternary ops
 	bool no_bail = test(&status, NOBAIL);
+
+	// Verbose output
+	if(test(&status, VERBOSE))
+	{
+		printf("%s: dirname is %s\n", programname, dirname);
+		printf("%s: pattern is %s\n", programname, pattern);
+		printf("%s: suffix_width is %d\n", programname, suffix_width);
+		printf("%s: no_bail is %s\n", programname, (no_bail) ? "true" : "false");
+		printf("%s: no_pat is %s\n", programname, (test(&status, NOPAT)) ? "true" : "false");
+		printf("%s: preview is %s\n", programname, (test(&status, PREVIEW)) ? "true" : "false");
+		printf("%s: first suffix is %s\n", programname, (test(&status, FIRSTSUF)) ? "true" : "false");
+		putchar('\n');
+	}
 
 	// Setting full directory path ahead of time instead of overwriting the same
 	// memory locations over and over again
@@ -163,7 +214,7 @@ int main(int argc, char *argv[])
 
 		// Assemble the new name
 		if(i == 0) ret = snprintf(newname, bufsize, "%s", pattern);
-		else	   ret = snprintf(newname, bufsize, "%s%*ld", pattern, suffix_width, i);
+		else	   ret = snprintf(newname, bufsize, "%s%0*ld", pattern, suffix_width, i);
 
 		if(ret >= bufsize - 1)
 		{
@@ -174,9 +225,12 @@ int main(int argc, char *argv[])
 		}
 
 		// A bunch of debugging prints
-		printf("realpath\t%s\n", fnindex);
-		printf("basename\t%s\n", direntobj->d_name);
-		printf("renamed to\t%s\n", newname);
+		if(test(&status, VERBOSE))
+		{
+			printf("realpath\t%s\n", fnindex);
+			printf("basename\t%s\n", direntobj->d_name);
+			printf("renamed to\t%s\n\n", newname);
+		}
 	}
 
 	return 0;
